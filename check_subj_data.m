e8f41d6ead5230cf1f8b648b218cb543e3f3a2f3
DanielNScott <dnscott87@gmail.com>
2017-01-24 23:03:36 -0500

    More diagnostics, 14-task -> 18-survey CCA

--- check_subj_data.m
+++ check_subj_data.m
@@ -1,12 +1,24 @@
-% Check SST Data
+% This file checks the SST data found in a .csv file with the
+% format specified in the BEESTS program.
+% 
+% To use it, load such a csv file into the appropriate 5 vectors:
+%  - inhibited
+%  - rt
+%  - ss_presented
+%  - ssd
+%  - subj_idx
 
-ss_presented(ss_presented == -999) = NaN;
-ssd(ssd == -999) = NaN;
+plot_flag = 1;
+suspect_ids = [175, 183];
+
+% Convert missing data to NaNs:
 rt(rt == -999) = NaN;
+ssd(ssd == -999) = NaN;
 inhibited(inhibited == -999) = NaN;
+ss_presented(ss_presented == -999) = NaN;
 
 max_len = 0;
-for i = 1:subj_idx(end)
+for i = suspect_ids(1):1:suspect_ids(2)
    subj_msk = subj_idx == i;
    subj_rt = rt(subj_msk);
    subj_ss = ss_presented(subj_msk);
@@ -25,7 +37,7 @@ for i = 1:subj_idx(end)
    end
 end
 
-for i = 100:100 %subj_idx(end)
+for i = suspect_ids(1):suspect_ids(2)
    subj_msk = subj_idx == i;
    
    if sum(subj_msk) == 0;
@@ -37,20 +49,39 @@ for i = 100:100 %subj_idx(end)
    subj_rt  = rt(subj_msk);
    subj_inh = inhibited(subj_msk);
 
-   figure();
-   subplot(3,1,1)
-   plot(subj_ss, '.')
-   title(['subj ' num2str(i) ' ss'])
+   tf = sum(~subj_ss & isnan(subj_rt));
+   disp(['Potential Observed TFs for Subject ', num2str(i), ': ', num2str(tf)])
+   disp(['Number of Observed RTs for Subject ', num2str(i), ': ', num2str(length(~isnan(subj_rt)))])
    
-   subplot(3,1,2)
-   plot(subj_ssd, '.')
-   title(['subj ' num2str(i) ' ssd'])
-   
-   subplot(3,1,3)
-   plotyy(1:length(subj_rt), subj_rt, 1:length(subj_rt), subj_inh)
-   title(['subj ' num2str(i) ' rt and inhib'])
-   
-   %subplot(2,2,4)
-   %plot(subj_inh, '.')
-   %title(['subj ' num2str(i) ' inh'])
+   if plot_flag
+      figure();
+      subplot(3,2,1)
+      plot(subj_ss, '.')
+      title(['subj ' num2str(i) ' ss'])
+
+      subplot(3,2,3)
+      plot(subj_ssd, '.')
+      title(['subj ' num2str(i) ' ssd'])
+
+      subplot(3,2,5)
+      plotyy(1:length(subj_rt), subj_rt, 1:length(subj_rt), subj_inh)
+      title(['subj ' num2str(i) ' rt and inhib'])
+
+      edges = 0:50:1000;
+      
+      subplot(3,2,2)
+      N = histc(subj_rt(subj_inh == 0), edges);
+      bar(edges,N,'histc')
+      title(['subj ' num2str(i) ' successful inhib histogram'])
+      
+      subplot(3,2,4)
+      N = histc(subj_ssd, edges);
+      bar(edges,N,'histc')
+      title(['subj ' num2str(i) ' ssd histogram'])
+
+      subplot(3,2,6)
+      N = histc(subj_rt, edges);
+      bar(edges,N,'histc')
+      title(['subj ' num2str(i) ' rt histogram'])
+   end
 end
