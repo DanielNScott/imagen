73cb0dc7617a4b9436b35f349d0cdfa3518caf08
Daniel Scott <dnscott87@gmail.com>
2017-06-27 16:33:35 -0400

    fMRI stuff simplified, modularized, incorporated.

---
+++ preproc/fit_fmri_glm.r
@@ -0,0 +1,177 @@
+fit_fmri_glm <- function(fmri_data) {
+
+  # Contrast groupings
+  conditions <- as.integer(unique(fmri_data$task_key))
+  n_conds    <- length(conditions)
+
+  # fMRI analysis functions
+  library(fmri)
+  #library(MASS)
+
+  # Proportion of subjects w/ significant STN betas for STOP_SUCCESS
+  frac_go_sig   <- 0
+  frac_stop_sig <- 0
+
+  # Number of scans in this series -- varies by individual.
+  n_scans <- length(fmri_data$acts[!is.na(fmri_data$acts[,1]),1])
+
+  # Array for events convolved w/ HRF
+  conv_regs <- array(dim = c(n_scans, n_conds))
+
+  # Some conditions do not occur - they will be removed
+  bad_cond  <- c()
+
+  # List of onset times by condition
+  onset_list <- list(c(), c(), c(), c(), c(), c(), c())
+
+  # Fill the conv_regressors array for each condition
+  for (cond in conditions) {
+
+    # Masks for getting trial times
+    cond_msk <- fmri_data$task_outcome %in% cond
+
+    # If this condition doesn't occur, skip it.
+    if (sum(cond_msk) == 0) {
+        bad_cond <- cbind(bad_cond,cond)
+        next
+    }
+
+    # For some reason fmri.stimulus can't handle the zero.
+    # Only one of thousands of pts, so just 'fix' it.
+    if (fmri_data$task_times[cond_msk][1] == 0) {
+        fmri_data$task_times[cond_msk][1] <- 500
+    }
+
+    # Trial onset times and their durations - 0s for event design
+    onsets    <- fmri_data$task_times[cond_msk]
+    onsets    <- as.vector(na.omit(onsets/1000/2.2))
+    durations <- double(length(onsets))
+    #print(onsets)
+
+    # Get convolved regressor for condition
+    conv_regs[,cond] <- fmri.stimulus(scans = n_scans, onsets = onsets, duration = durations)
+
+    #print(conv_regs)
+    onset_list[[cond]] <- onsets
+  }
+  #print(bad_cond)
+  #print(conv_regs)
+
+  # Create design matrix w/ 2nd deg drift, remove any bad conditions
+  design_mat <- fmri.design(conv_regs[, setdiff(1:7, bad_cond)], order = 2)
+  #colnames(design_mat) <- c(cnames[setdiff(1:7, bad_cond)], 'Intercept', 'Linear Drift', 'Sq. Drift')
+
+  #design_mat <- fmri.design(conv_regs, order = 2)
+  #colnames(design_mat) <- c(cnames, 'Intercept', 'Linear Drift', 'Sq. Drift')
+
+  #print(design_mat)
+  #design_mat <- design_mat[,setdiff(1:7, bad_cond)]
+  #print(design_mat)
+
+  # Clean up conv_regs a bit, append 'TR' column, label things
+  conv_regs[,bad_cond] <- integer(n_scans)
+  conv_regs  <- data.frame(cbind(conv_regs, 1:n_scans))
+  #colnames(conv_regs) <- c(cnames,'TR')
+
+  # Fit the linear model
+  linear_model <- lm(fmri_data$acts ~ design_mat)
+
+  # Save the linear model, list of conds removed,
+  lm_list <- list(lm=linear_model, bad_cond=bad_cond, conv_regs=conv_regs, onsets = onset_list)
+
+  return(lm_list)
+
+  ##################################################
+  # The rest of this function needs dealing with!
+  ##################################################
+
+  # ------------------------- #
+  # Actual processing is done #
+  # ------------------------- #
+  # Now stuff is just getting printed
+  lm_desc <- summary(linear_model)
+  coeff   <- lm_desc$coefficients
+
+  # STOP_SUCCESS is coeff. 4 originally (cond 3)
+  stop_ind <- 4
+  if (2 %in% bad_cond) {
+    stop_ind <- stop_ind - 1
+  }
+
+  # Information
+  print(paste('Subject ', subj_num,
+            ' GO_SUCCESS beta: ', round(coeff[2,'Estimate'], digits=2),
+            '. Sig: ', coeff[2,'Pr(>|t|)'] < 0.05,
+            ' STOP_SUCCESS beta: ', round(coeff[stop_ind,'Estimate'], digits=2),
+            '. Sig: ', coeff[stop_ind,'Pr(>|t|)'] < 0.05, sep=''))
+
+  if (coeff[2,'Pr(>|t|)'] < 0.05) {
+    frac_go_sig <- frac_go_sig + 1
+  }
+  if (coeff[stop_ind,'Pr(>|t|)'] < 0.05) {
+    frac_stop_sig <- frac_stop_sig + 1
+  }
+
+  # Report on betas:
+  #frac_go_sig   <- frac_go_sig  /n_to_fit
+  #frac_stop_sig <- frac_stop_sig/n_to_fit
+
+  print('')
+  print(paste('Fraction of GO_SUCCESS   betas which are significant:', frac_go_sig))
+  print(paste('Fraction of STOP_SUCCESS betas which are significant:', frac_stop_sig))
+
+  return (lm_list)
+}
+
+
+fir <- function(onsets, durations, activations, n_scans, n_conds, n_voxels) {
+  # Scale parameter - sets resolution
+  scale <- 10
+
+  # Rescale onsets
+  onsets    <- onsets    * scale
+  durations <- durations * scale
+
+  n_scans <- n_scans * scale
+  res     <- TR / scale
+
+  #if (type == "user")
+  #   shrf <- sum(hrf(0:(ceiling(scans) - 1)/scale))
+  #   no  <- length(onsets)
+  #if (length(durations) == 1) {
+  #  durations <- rep(durations, no)
+  #}
+  #else if (length(durations) != no) {
+  #  stop("Length of duration vector does not match the number of onsets!")
+  #}
+  stims <- rep(0, ceiling(scans))
+
+  ## ESTIMATE HRF USING FIR BASIS SET
+
+  # CREATE FIR DESIGN MATRIX
+  # WE ASSUME HRF IS 16 TRS LONG
+  hrf_len <- 16
+
+  # BASIS SET FOR EACH CONDITOIN IS A TRAIN OF INPULSES
+  fir_bases <- zeros(n_scans, hrf_len*n_conds)
+
+  for (cond in 1:n_conds) {
+      col_subset <- ((cond - 1)* hrf_len + 1):(cond*hrf_len)
+
+      for (onset in 1:numel(onsets[,cond]) ) {
+          #impulse_times <- onsets(onset):onsets(onset) + hrf_len - 1;
+          impulse_times <- seq(onsets(onset), onsets(onset) + hrf_len*scale - 1, scale)
+
+          for (impulse in 1:numel(impulse_times)) {
+              fir_bases(impulse_times(impulse), col_subset(impulse)) <- 1;
+          }
+      }
+  }
+
+  # ESTIMATE HRF FOR EACH CONDITION AND VOXEL
+  fir_hrf_est <- pseudoinverse(fir_bases %*% fir_bases) * fir_bases %*% activations
+
+  # RESHAPE HRFS
+  hHatFIR <- reshape(fir_hrf_est, hrf_len, n_conds, n_voxels)
+
+}
--- preproc/import_all.r
+++ preproc/import_all.r
@@ -72,17 +72,126 @@ read_mid_param_file <- function(filename){
 
 
 # ------------------------------------------------------------------------------ #
+#                      Subroutine for reading the fmri data                      #
+# ------------------------------------------------------------------------------ #
+import_fmri <- function(subj_id, timeseries_dir, taskdata_dir) {
+
+  # Set filename prefixes and suffixes
+  task_pfx <- 'ss_'
+  fmri_sfx <- paste('MaskDumpOutput',sep='')
+
+  # Specify ordinal coding for trial outcome
+  task_key  <- c("GO_SUCCESS"="1", "GO_FAILURE"='2', 'STOP_SUCCESS'='3', 'STOP_FAILURE'='4',
+                   'GO_TOO_LATE'='5','GO_WRONG_KEY_RESPONSE'='6', 'STOP_TOO_EARLY_RESPONSE'='7')
+
+  #failed_fmri_reads <- 0
+  #failed_task_reads <- 0
+
+  #failed_fmri_cols  <- c()
+  #failed_task_cols  <- c()
+
+  # Convert subject id to padded string & read
+  subj_id_str <- formatC(subj_id, width = 12, format = 'd', flag = '0')
+
+  fmri_fname <- paste(subj_id_str, '_', fmri_sfx, sep='')
+  task_fname <- paste(task_pfx, subj_id_str, '.csv', sep='')
+
+  fmri_full_name <- paste(timeseries_dir, fmri_fname, sep='')
+  task_full_name <- paste(taskdata_dir  , task_fname, sep='')
+
+  # Reading the fmri Series
+  if (file.exists(fmri_full_name)) {
+    activations <- read.csv(file=fmri_full_name, header=FALSE, sep=' ')
+    activations <- t(activations[,4:447])
+    rownames(activations) <- NULL
+    print(paste('Successful read of ', fmri_fname))
+  } else {
+
+    print(paste('Failure to read ', fmri_fname))
+    return
+    #failed_fmri_reads <- failed_fmri_reads + 1
+    #failed_fmri_cols  <- c(failed_fmri_cols, subj_num)
+  }
+
+  # Reading the SST Series
+  if (file.exists(task_full_name)) {
+    # Only want to read 2 columns, time and outcome
+    cols <- c('NULL','NULL','numeric','NULL','NULL','NULL','NULL','NULL',
+              'NULL','NULL','NULL'   , NA   ,'NULL','NULL','NULL','numeric')
+    task_data <- read.csv(file = task_full_name, header = TRUE, sep = '\t', colClasses = cols, skip = 1)
+    print(paste('Successful read of ', task_fname))
+  } else {
+    print(paste('Failure to read ', task_fname))
+    return
+    #failed_task_reads <- failed_task_reads + 1
+    #failed_task_cols  <- c(failed_task_cols, subj_num)
+  }
+
+
+  # Req. library for revalue()
+  library(plyr)
+
+  # Extract the trial times and outcomes
+  task_times <- task_data['Trial.Start.Time..Onset.'][,1]
+  task_data['Response.Outcome'][,1] <- revalue(task_data['Response.Outcome'][,1], task_key, warn_missing=FALSE)
+  task_outcome <- as.numeric(as.character(task_data['Response.Outcome'][,1]))
+
+
+  #print(paste('Completed ', n_train - failed_fmri_reads, 'out of', n_train, 'fmri reads.', sep=' '))
+  #print(paste('Completed ', n_train - failed_task_reads, 'out of', n_train, 'SST reads.', sep=' '))
+
+  #lost_cols  <- c(failed_fmri_cols, failed_task_cols)
+  #valid_cols <- setdiff(1:subj_num, lost_cols)
+
+  # Format the fmri data as a dataframe and add TRs
+  activations <- as.matrix(activations)
+  trs <- seq(1,444)
+
+  # Save the fmri_series data (for raw <-> processed comparison)
+  saveRDS(activations, 'activations.rds')
+
+  # activations <- activations[,1:3]
+  # Z-Score the data for each participant
+  mean <- colMeans(activations, na.rm=TRUE)
+  std  <- apply(activations, 2, sd, na.rm = TRUE)
+
+  activations <- sweep(activations, 2, mean, FUN = "-")
+  activations <- sweep(activations, 2, std , FUN = "/")
+
+  # Spike removal
+  spike_inds <- which(abs(activations) >= 3)
+  activations[spike_inds] <- 0
+
+  # Drift correction
+  polyfit     <- lm( as.matrix(activations) ~ poly( as.vector(trs), 2))
+  activations <- activations - polyfit$fitted.values
+
+  # High-Pass Filter
+  bf <- signal::butter(2, 1/128, type="high")
+  filter <- function(x) {signal::filtfilt(bf, x)}
+  activations <- apply(activations, 2, filter)
+
+  # Should drift correct and Z-Score again after filtering
+
+  data <- list(acts = activations, task_outcome = task_outcome, task_times = task_times, task_key = task_key)
+  return (data)
+
+} # EOF
+# ------------------------------------------------------------------------------ #
+
+
+# ------------------------------------------------------------------------------ #
 #                Master routine for creating 'data' variable
 # ------------------------------------------------------------------------------ #
 import_all <- function(){
   library('futile.logger')
 
   # Where is data located, and who are the subjects?
-  base_dir      <- '/home/dan/projects/imagen/data/'
+  base_dir      <- '/gpfs/home/dscott3/projects/imagen/data/'
   sbx_subj_list <- paste(base_dir, 'sandbox_subject_list.csv', sep = '')
 
   # Read subject list & demarcate test/training split
-  data <- data.frame()
+  data <- c()
   subj_list <- read.csv(sbx_subj_list, header = TRUE, sep = ',')
   data$train_inds <- 1:198
   data$test_inds  <- 199:396
@@ -122,28 +231,37 @@ import_all <- function(){
   #
   # Reading in more than one subjects complete FMRI data at a time would be
   # impossibly memory intensive
-  taskdata_ dir  <- paste('/home/dan/documents/lncc/From Catherine/Round 2/SST/SST_', visit,'_behavioraldata/',sep='')
-  timeseries_dir <- paste('/home/dan/documents/lncc/From Catherine/', visit, '/',roi,'_',visit,'_SSTtimeseries/', sep='')
+  visit <- 'BL'
+  task  <- 'SST'
+  taskdata_dir   <- paste(base_dir, task, '_', visit, '_behavioraldata/', sep='')
+  timeseries_dir <- paste(base_dir, visit, '_VoxelLevel_', task, '/', sep='')
 
-  for (id in subj_list) {
-    fmri_data <- import_fmri(id, 'SST', timeseries_dir, taskdata_dir)
-    sst_betas <- get_fmri_betas(fmri_data)
+  for (id in subj_list[1:5]) {
+    fmri_data <- import_fmri(id, timeseries_dir, taskdata_dir)
+    sst_betas <- fit_fmri_glm(fmri_data)
   }
 
-  # Get MID FMRI beta values #### DOES NOT EXIST YET ###
+  #jpeg('rplot.jpg')
+  #plot(x,y)
+  #dev.off()
+
+  #### DOES NOT EXIST YET ###
+  # Get MID FMRI beta values
   #mid_fmri_data <- import_fmri()
-  #mid_betas <- get_fmri_betas(mid_fmri_data)
+  #mid_betas <- fit_fmri_glm(mid_fmri_data)
 
 
+  ### DOES NOT EXIST YET
   ### ---------------------- ###
-  ###   Clinical & Genetic   ### DOES NOT EXIST YET
+  ###   Clinical & Genetic   ###
   ### ---------------------- ###
   #clinical_data <- import_clinical()
   #genetic_data  <- import_genetic()
 
 
+  ### NEEDS UPDATE
   # Convert annoying names into better ones
-  data <- replace_bad_names(sst_params, mid_params, raw_df)
+  #data <- replace_bad_names(sst_params, mid_params, raw_df)
 
   return(data)
 }
